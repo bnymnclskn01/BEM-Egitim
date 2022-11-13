66a844f4a36eda648ba58cd623054d471cd01337
360MEKA <bnymncliskn1903@gmail.com>
2022-11-13 10:23:48 +0300

    Düzenleme Alındı

--- MVC/MVCDERSUI/MVCDERSUI/Controllers/UserController.cs
+++ MVC/MVCDERSUI/MVCDERSUI/Controllers/UserController.cs
@@ -28,5 +28,50 @@ namespace MVCDERSUI.Controllers
             context.SaveChanges();
             return RedirectToAction("Index");
         }
+        [HttpGet("User/Edit/{id}")]
+        public IActionResult Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return RedirectToAction("Index");
+            }
+            var data = context.UserMembers.Find(id);
+            if (data == null)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(data);
+        }
+        [HttpPost("User/Edit/{id}")]
+        public IActionResult Edit(Guid id, UserMember userMember)
+        {
+            var data = context.UserMembers.Find(id);
+            if (ModelState.IsValid)
+            {
+                data.NameSurname = userMember.NameSurname;
+                data.LastDate = DateTime.Now;
+                data.Email = userMember.Email;
+                data.Password = userMember.Password;
+                data.UserNote = userMember.UserNote;
+                context.UserMembers.Update(data);
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(data);
+            }
+        }
+        public IActionResult Delete(Guid id)
+        {
+            var data = context.UserMembers.Find(id);
+            if (ModelState.IsValid)
+            {
+                context.UserMembers.Remove(data);
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(data);
+        }
     }
 }
