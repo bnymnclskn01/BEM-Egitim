48591f0065b64a12636fb0526874b8a4f4f31e5d
360MEKA <bnymncliskn1903@gmail.com>
2022-11-19 12:05:26 +0300

    Product Kategory İşlemleri Devam Ediyor

---
+++ MVC/MVCDERSUI/MVCDERSUI/Controllers/ProductCategoryController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCDERSUI.Models.DatabaseModelContext;
+using MVCDERSUI.Models.Entity;
+
+namespace MVCDERSUI.Controllers
+{
+    public class ProductCategoryController : Controller
+    {
+        private MvcDersDatabaseModelContext context = new MvcDersDatabaseModelContext();
+        [HttpGet("admin/urun-kategori-listele")]
+        public IActionResult Index()
+        {
+            var data = context.ProductCategories.OrderBy(x => x.Rank).ToList();
+            return View(data);
+        }
+
+        [HttpGet("admin/urun-kategori-ekle")]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost("admin/urun-kategori-ekle")]
+        public IActionResult Create(ProductCategory productCategory)
+        {
+            var control = context.ProductCategories.Where(x => x.CategoryName == productCategory.CategoryName).FirstOrDefault();
+            if (ModelState.IsValid)
+            {
+                if (control != null)
+                {
+                    ViewBag.Hata = "Girdiğiniz kayı sistemimizde mevcut olduğundan dolayı ekleme işlemi yapamıyoruz. Kayıt Bilgilerini Kontrol Edip Tekrar Deneyiniz.";
+                    return View(productCategory);
+                }
+                else
+                {
+                    productCategory.CreateDate = DateTime.Now;
+                    productCategory.LastDate = DateTime.Now;
+                    productCategory.Status = true;
+                    context.ProductCategories.Add(productCategory);
+                    context.SaveChanges();
+                    return RedirectToAction(nameof(Index));
+                }
+                return View(productCategory);
+            }
+        }
+    }
+}
---
+++ MVC/MVCDERSUI/MVCDERSUI/Controllers/ProductController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCDERSUI.Models.DatabaseModelContext;
+
+namespace MVCDERSUI.Controllers
+{
+
+    public class ProductController : Controller
+    {
+        private MvcDersDatabaseModelContext context = new MvcDersDatabaseModelContext();
+
+        [HttpGet("admin/urun-listele")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
